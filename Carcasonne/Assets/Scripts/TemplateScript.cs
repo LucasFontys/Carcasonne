325e114d81a82fe56ac935ee4830b70158a2389c
LucasDesktop <lucas-derksen@hotmail.com>
2020-01-03 15:26:04 +0100

    First Commit

---
+++ Carcasonne/Assets/Scripts/TemplateScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateScript : MonoBehaviour
+{
+    public GameObject grabbedObject;
+
+
+    private bool amHolding = false;
+
+    private Vector2 mousePos;
+
+    public LayerMask placedTiles;
+    public LayerMask grabableTiles;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if something grabbable under
+        Vector2 mouseRay = Camera.main.ScreenToWorldPoint(mousePos);
+        RaycastHit2D rayHitGrabbable = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, grabableTiles);
+        RaycastHit2D rayHitPlaced = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity, placedTiles);
+
+        // mouse position 
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (amHolding)
+        {
+            
+            grabbedObject.transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                RotateRight();
+            }
+        }      
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (amHolding)
+            {
+                if (rayHitPlaced.collider == null)
+                {
+                    //drop held object
+                    grabbedObject.layer = 9;
+
+                    grabbedObject = null;
+                    amHolding = false;
+                }
+                else
+                {
+                    //cant drop
+                    Debug.Log("cant drop");
+                }
+              
+            }
+            else{
+                
+
+                if(rayHitGrabbable.collider == null)
+                {
+                    // niks gevonden om op te pakken
+                    Debug.Log("Miss");
+                }
+                else
+                {
+                    Debug.Log("Hit");
+                    grabbedObject = rayHitGrabbable.collider.gameObject;
+                    amHolding = true;
+                }
+
+
+            }
+        }
+
+        
+
+        
+    }
+
+    void RotateRight()
+    {
+        Debug.Log("Turn right");
+        grabbedObject.transform.Rotate(Vector3.forward * -90);
+    }
+}
---
+++ Carcasonne/Assets/moveCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveCamera : MonoBehaviour
+{
+    public Button upButton;
+    public Button downButton;
+    public Button leftButton;
+    public Button rightButton;
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btnUP = upButton.GetComponent<Button>();
+        Button btnDOWN = downButton.GetComponent<Button>();
+        Button btnLEFT = leftButton.GetComponent<Button>();
+        Button btnRIGHT = rightButton.GetComponent<Button>();
+        btnUP.onClick.AddListener(MoveUp);
+        btnDOWN.onClick.AddListener(MoveDown);
+        btnLEFT.onClick.AddListener(MoveLeft);
+        btnRIGHT.onClick.AddListener(MoveRight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveRight()
+    {
+        Debug.Log("You have clicked the button!");
+        mainCamera.transform.position = mainCamera.transform.position + new Vector3(4, 0);
+    }
+
+    void MoveLeft()
+    {
+        Debug.Log("You have clicked the button!");
+        mainCamera.transform.position = mainCamera.transform.position + new Vector3(-4, 0);
+    }
+
+    void MoveUp()
+    {
+        Debug.Log("You have clicked the button!");
+        mainCamera.transform.position = mainCamera.transform.position + new Vector3(0, 4);
+    }
+
+    void MoveDown()
+    {
+        Debug.Log("You have clicked the button!");
+        mainCamera.transform.position = mainCamera.transform.position + new Vector3(0, -4);
+    }
+}
